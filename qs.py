4fef7b01acc4c165c2cd4155980d7b51fdcfdf0f
Jonan Seeley <jonans99@yahoo.com>
2019-05-29 23:22:35 -0400

    initial prototype and beginning site code

--- qs.py
+++ qs.py
@@ -5,5 +5,26 @@ with open("jawn.html") as f:
 
 table = soup.find_all(id="Tab1")[0]
 tds = table.find_all("td")
-print(len(tds))
-#print(tds[2], tds[19], tds[19+17])
+
+questions = []
+answers = []
+for i in range(2, len(tds), 17):
+    text = tds[i].p.contents[0].split(" ")
+    questions.append(" ".join(text[0:-1]))
+    answers.append(text[-1][1:-1])
+
+jawn = "\", \"".join(questions)
+print(f"[\"{jawn}\"]")
+
+correct = 0
+'''
+for i, (q, a) in enumerate(zip(questions, answers)):
+    print(q)
+    x = input()
+    if x == a:
+        print("Correct!")
+        correct += 1
+    else:
+        print(f'Your answer: {x}\tCorrect answer: {a}')
+    print(f"You have gotten {correct} out of {i+1} correct!\n") 
+    '''
